156ce14bb3b89968596bddaf2d7ce33c8e76a208
lovebai <1507820372@qq.com>
2019-10-25 22:17:09 +0800

    忘记保存了

--- emoji/emoji_bak.py
+++ emoji/emoji_bak.py
@@ -5,17 +5,19 @@ import os
 
 def open_url():
     page = int(input("请输入要下载的页数然后回车即可："))
-    url = "https://www.fabiaoqing.com/biaoqing/lists/page/"+str(page)+".html"
-    response = requests.get(url)
-    html = response.text
-    sel = parsel.Selector(html)
-    divs = sel.css('.tagbqppdiv')
-    urls = []
-    for div in divs:
-        img_url = div.css('img.ui::attr(data-original)').getall()
-        img_title = div.css('img.ui::attr(title)').getall()
-        urls.append((img_url,img_title))        
-    return urls
+    for i in range(0,page):   
+        pages = ++i
+        url = "https://www.fabiaoqing.com/biaoqing/lists/page/"+str(pages)+".html"
+        response = requests.get(url)
+        html = response.text
+        sel = parsel.Selector(html)
+        divs = sel.css('.tagbqppdiv')
+        urls = []
+        for div in divs:
+            img_url = div.css('img.ui::attr(data-original)').getall()
+            img_title = div.css('img.ui::attr(title)').getall()
+            urls.append((img_url,img_title))        
+        return urls
 
 
 def save_img(folder):
@@ -34,7 +36,8 @@ def save_img(folder):
 def download_emoji(folder="emoji"):
     print("本程序会自动采集表情网【fabiaoqing.com】的热门分类表情包")
     print("表情包会保存到和本程序同一目录下的emoji文件夹里面")
-    print("打开链接地址：https://www.fabiaoqing.com/biaoqing查看要下载页面的页码")
+    print("距今2019/10/25此分类页只有200页所以在下面的下载页数中最多输入200\n如果之后的日期就打开网站看看")
+    print("链接地址：https://www.fabiaoqing.com/biaoqing")
     if os.path.exists('emoji'):
         pass
     else:
